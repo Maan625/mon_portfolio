8744968b576a517bc1abba785d77548bd4ecc1d7
AL ALI <mz777778@gmail.com>
2025-07-28 19:34:08 +0200

    terminer la page a propre

---
+++ scss/main.js
@@ -0,0 +1,5 @@
+const carte_presantation= document.querySelector(".titre_portfolio");
+const button_savoir = document.getElementById('button_savoir');
+button_savoir.addEventListener('click', () => {
+     carte_presantation.style.display="block";
+});
